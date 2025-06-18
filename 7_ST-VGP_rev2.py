ee7d50adfd15b53cf33abdaa8c8f235241a1cb23
yilmajung <woo.firstrunner@gmail.com>
2025-06-18 15:03:44 -0400

    change kernel structure

--- 7_ST-VGP_rev2.py
+++ 7_ST-VGP_rev2.py
@@ -105,7 +105,7 @@ class STVGPModel(gpytorch.models.ApproximateGP):
         temporal_x = x[:, 2:3]
         covariate_x = x[:, 3:]
         mean_x = self.mean_module(covariate_x)
-        covar_x = self.spatial_kernel(spatial_x) * self.temporal_kernel(temporal_x) * self.covariate_kernel(covariate_x)
+        covar_x = self.spatial_kernel(spatial_x) * self.temporal_kernel(temporal_x) + self.covariate_kernel(covariate_x)
         covar_x = covar_x + torch.eye(covar_x.size(-1), device=x.device) * 1e-2
 
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
