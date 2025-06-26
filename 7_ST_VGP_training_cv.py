dc96e02f534ca1a4ed24adcac41298268b6d036f
yilmajung <woo.firstrunner@gmail.com>
2025-06-26 11:10:49 -0400

    update avail gpu ids

--- 7_ST_VGP_training_cv.py
+++ 7_ST_VGP_training_cv.py
@@ -274,7 +274,7 @@ def evaluate_params(params):
 # Wrapper to pin each process to a different GPU
 def evaluate_on_gpu(params, idx):
     # Round-robin GPU assignment
-    gpu_ids = list(range(torch.cuda.device_count()))
+    gpu_ids = [2, 3, 4, 5, 6, 7] #list(range(torch.cuda.device_count()))
     n_gpus = len(gpu_ids)
     device_id = gpu_ids[idx % n_gpus]
     torch.cuda.set_device(device_id)
@@ -295,7 +295,7 @@ grid = list(ParameterGrid(param_grid))
 print(f"Total combinations: {len(grid)}")
 
 # Number of GPUs to use
-gpu_ids = list(range(torch.cuda.device_count()))
+gpu_ids = [2, 3, 4, 5, 6, 7] #list(range(torch.cuda.device_count()))
 n_gpus = len(gpu_ids)
 
 with tqdm_joblib(tqdm(desc="Grid search", total=len(grid))):
