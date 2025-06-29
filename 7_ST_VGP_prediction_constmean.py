4fc64485ddeffc01aa0d741d7bb6594b7f4b809d
yilmajung <woo.firstrunner@gmail.com>
2025-06-29 09:53:20 -0400

    fix variable names

--- 7_ST_VGP_prediction_constmean.py
+++ 7_ST_VGP_prediction_constmean.py
@@ -176,10 +176,10 @@ with torch.no_grad(), gpytorch.settings.fast_pred_var():
         y_samps = nb.sample()  # (S, B)
         y_np    = y_samps.cpu().numpy()
 
-        lower_95_all[offset:offset+B_i] = np.percentile(samples, 2.5, axis=0)
-        upper_95_all[offset:offset+B_i] = np.percentile(samples,97.5, axis=0)
-        lower_90_all[offset:offset+B_i] = np.percentile(samples, 5.0, axis=0)
-        upper_90_all[offset:offset+B_i] = np.percentile(samples,95.0, axis=0)
+        lower_95_all[offset:offset+B_i] = np.percentile(y_np, 2.5, axis=0)
+        upper_95_all[offset:offset+B_i] = np.percentile(y_np,97.5, axis=0)
+        lower_90_all[offset:offset+B_i] = np.percentile(y_np, 5.0, axis=0)
+        upper_90_all[offset:offset+B_i] = np.percentile(y_np,95.0, axis=0)
 
         offset += B_i
 
