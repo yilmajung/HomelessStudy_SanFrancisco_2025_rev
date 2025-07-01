4bb6b8e67ac4ae5862734261a6437c141da0a1c0
yilmajung <woo.firstrunner@gmail.com>
2025-07-01 15:52:57 -0400

    fix scaler issue

--- 7_ST_VGP_prediction_aggregation_ip500.py
+++ 7_ST_VGP_prediction_aggregation_ip500.py
@@ -92,27 +92,6 @@ df_test = df[df['ground_truth'].isna()]
 # Small subset for testing
 # df_test = df_test.sample(n=1000, random_state=42).reset_index(drop=True)
 
-#=====================
-df_training = df.dropna(subset=['ground_truth'])
-
-# Extract coordinates and covariates
-spatial_coords = df_training[['latitude', 'longitude']].values
-temporal_coords = df_training[['timestamp']].values
-X_covariates = df_training[['max','min','precipitation','total_population','white_ratio','black_ratio','hh_median_income']]
-y_counts = df_training['ground_truth'].values
-
-# Stack and scale
-print("Preparing training tensors...")
-t = np.hstack((spatial_coords, temporal_coords, X_covariates))
-y = y_counts.astype(np.float32)
-scaler = StandardScaler().fit(t)
-x_scaled = scaler.transform(t).astype(np.float32)
-
-# Prepare tensors
-train_x = torch.tensor(x_scaled, dtype=torch.float32)
-train_y = torch.tensor(y, dtype=torch.float32)
-#=====================
-
 
 # Instantiate and load trained parameters
 model = STVGPModel(inducing_points.to(device), constant_mean=constant_mean).to(device)
